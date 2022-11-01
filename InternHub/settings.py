c7a0ccb0ecb21458c226a3762d0d864fdf55855f
MiniThought17 <felixomwenga441@gmail.com>
2022-11-01 20:53:27 +0300

    Debug mode off

--- InternHub/settings.py
+++ InternHub/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-in1q(e0bfx%iwremxm9l$1m%=r-)x&vi1sgf&&9orqmvax&_j0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
